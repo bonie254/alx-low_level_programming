8c9e3657b5a7b696707d62363700846d8562d000
bonie254 <kdtum0344@gmail.com>
2023-10-07 07:19:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * maim - Aprogram that prints aline with puts function
- * return :0 (success)
- */
-int main(void)
-{       
-put("\"Programming is like building a multilingual puzzle");
+ * main - Aprogram that prints aline with puts function
+ * Return :0 (success)
+ * int main(void){
+
+puts("\"Programmig is like building a multilingual puzzle");
 return (0);
 }
