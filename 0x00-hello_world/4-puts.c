ac29fac3146f0f59fff6cf38fa51d4665bfd0131
bonie254 <kdtum0344@gmail.com>
2023-10-07 05:11:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * maim - Aprogram that prints aline with puts function
+ * return :0 (success)
+ */
+int main(void)
+{       
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
